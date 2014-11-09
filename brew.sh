c0ccf3a80d9dc0acbedfa7b2758c5f3a655c1c3c
Niklas Lavrell <niklas.lavrell@accedo.tv>
2014-11-09 18:37:02 +0100

    added rupa/z

--- brew.sh
+++ brew.sh
@@ -94,20 +94,5 @@ brew install node
 
 #brew install homebrew/versions/lua52
 
-# Install HomeBrew Cask
-brew install caskroom/cask/brew-cask
-
-#brew cask install google-chrome
-#brew cask install firefox
-#brew cask install sublime-text
-#brew cask install iterm2
-#brew cask install spotify
-#brew cask install dropbox
-#brew cask install google-drive
-#brew cask install skype
-#brew cask install slack
-#brew cask install vlc
-#brew cask install flux
-
 # Remove outdated versions from the cellar.
 brew cleanup
