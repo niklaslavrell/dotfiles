1dedc7588c3c4e3cd27b16da6fe16ed031c8fc0c
Niklas Lavrell <niklas.lavrell@accedo.tv>
2014-11-09 20:45:33 +0100

    added dependency and apps scripts

---
+++ install-apps.sh
@@ -0,0 +1,14 @@
+# Install HomeBrew Cask
+brew install caskroom/cask/brew-cask
+
+#brew cask install google-chrome
+#brew cask install firefox
+#brew cask install sublime-text
+#brew cask install iterm2
+#brew cask install spotify
+#brew cask install dropbox
+#brew cask install google-drive
+#brew cask install skype
+#brew cask install slack
+#brew cask install vlc
+#brew cask install flux
