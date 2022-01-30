904b43d9afed93d8b9c615f07ac5bf7e5275695f
Niklas Lavrell <niklas.lavrell@gmail.com>
2022-01-30 11:16:20 +0100

    early 2022 updates

--- install.sh
+++ install.sh
@@ -12,17 +12,9 @@ brew upgrade
 brew install n
 brew install yarn
 brew install mas
-brew install lastpass-cli
-# brew install tree
 brew install git
 brew install git-lfs
-# brew install node
-# brew install ffmpeg
-# brew install mongodb
-# brew install heroku
-# brew install docker
-# brew install docker-machine
-# brew install docker-compose
+brew install ffmpeg
 
 # Remove outdated versions from the cellar.
 brew cleanup
@@ -31,45 +23,21 @@ brew cleanup
 # Installs some nice native apps with Homebrew Cask                           #
 ###############################################################################
 
-# Install Homebrew Cask
-# brew install caskroom/cask/brew-cask
-# brew tap caskroom/versions
-
-# daily
-# brew cask install flux
-brew cask install google-backup-and-sync
-# brew cask install rocket
-# brew cask install dropbox
-
-# development
-brew cask install iterm2
-brew cask install visual-studio-code
-# brew cask install docker
-# brew cask install postman
-# brew cask install figma
-
-# chat
-brew cask install slack
-# brew cask install franz
-# brew cask install skype
-
-# browsers
-brew cask install google-chrome
-# brew cask install firefox
-
-# other
-brew cask install spotify
-brew cask install notion
-# brew cask install tunnelbear
-# brew cask install there
-# brew cask install toggl
+brew install --cask google-drive
+brew install --cask dropbox
+brew install --cask iterm2
+brew install --cask visual-studio-code
+brew install --cask figma
+brew install --cask slack
+brew install --cask google-chrome
+brew install --cask spotify
+brew install --cask notion
 
 ###############################################################################
 # Install tools from Yarn                                                     #
 ###############################################################################
 
 yarn global add create-react-app
-yarn global add emoj
 yarn global add firebase-tools
 yarn global add gatsby-cli
 yarn global add netlify-cli
@@ -81,7 +49,6 @@ yarn global add netlify-cli
 mas lucky Xcode
 mas lucky Messenger
 mas lucky Todoist
-mas lucky LastPass
 
 ###############################################################################
 # Install Oh My Zsh                                                           #
